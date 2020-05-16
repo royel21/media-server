aecd7c67336ba89f400c3750cea76439c1e42550
royel21 <royelconsoro@hotmail.com>
2020-05-15 20:57:41 -0400

    fix file name too long from column size

--- models/file.js
+++ models/file.js
@@ -1,59 +1,65 @@
 const { nanoid } = require("nanoid");
 module.exports = (sequelize, DataTypes) => {
-  const { INTEGER, STRING, DATE, FLOAT } = DataTypes;
-  const File = sequelize.define(
-    "File",
-    {
-      Id: {
-        type: STRING(10),
-        primaryKey: true,
-        unique: true,
-        allowNull: false,
-      },
-      Name: {
-        type: STRING(150),
-      },
-      Duration: {
-        type: FLOAT(8, 2).UNSIGNED,
-        defaultValue: 0,
-      },
-      Type: {
-        type: STRING(25),
-        defaultValue: "",
-        allowNull: false,
-      },
-      Size: {
-        type: INTEGER,
-        allowNull: true,
-      },
-      CreatedAt: {
-        type: DATE,
-      },
-      ViewCount: {
-        type: INTEGER,
-        defaultValue: 0,
-      },
-      Cover: {
-        type: STRING,
-      },
-    },
-    {
-      timestamps: false,
-      hooks: {
-        beforeValidate: function (item) {
-          item.Id = nanoid(10);
-          if (item.Name && item.Type)
-            item.Cover = `/${item.Type}/${item.Name.replace(/#|%/gi, "")}.jpg`;
+    const { INTEGER, STRING, DATE, FLOAT } = DataTypes;
+    const File = sequelize.define(
+        "File",
+        {
+            Id: {
+                type: STRING(10),
+                primaryKey: true,
+                unique: true,
+                allowNull: false,
+            },
+            Name: {
+                type: STRING,
+            },
+            Duration: {
+                type: FLOAT(8, 2).UNSIGNED,
+                defaultValue: 0,
+            },
+            Type: {
+                type: STRING(10),
+                defaultValue: "",
+                allowNull: false,
+            },
+            Size: {
+                type: INTEGER,
+                allowNull: true,
+            },
+            CreatedAt: {
+                type: DATE,
+            },
+            ViewCount: {
+                type: INTEGER,
+                defaultValue: 0,
+            },
+            Cover: {
+                type: STRING,
+            },
         },
-        beforeBulkCreate(instances) {
-          for (var item of instances) {
-            item.Id = nanoid(10);
-            item.Cover = `/${item.Type}/${item.Name.replace(/#|%/gi, "")}.jpg`;
-          }
-        },
-      },
-    }
-  );
+        {
+            timestamps: false,
+            hooks: {
+                beforeValidate: function (item) {
+                    item.Id = nanoid(10);
+                    if (item.Name && item.Type)
+                        item.Cover = `/${item.Type}/${item.Name.replace(
+                            /#|%/gi,
+                            ""
+                        )}.jpg`;
+                },
+                beforeBulkCreate(instances) {
+                    for (var item of instances) {
+                        item.Id = nanoid(10);
+                        item.Cover = `/${item.Type}/${item.Name.replace(
+                            /#|%/gi,
+                            ""
+                        )}.jpg`;
+                    }
+                },
+            },
+        }
+    );
 
-  return File;
+    return File;
 };
