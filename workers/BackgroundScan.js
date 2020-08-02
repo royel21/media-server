93bc3570c74caf33e740f5f76c68417c9cdda5a4
Royel Consoro <royelconsoro@hotmail.com>
2020-08-02 02:55:06 +0000

    remove orphan folder

--- workers/BackgroundScan.js
+++ workers/BackgroundScan.js
@@ -25,14 +25,10 @@ const createFolderAndCover = async (dir, files, fd) => {
     if (!firstFile) return "";
     let Name = path.basename(dir);
     let FolderCover = path.join(coverPath, Name + ".jpg");
-    let FilesType = /\.(rar|zip)/gi.test(firstFile.FileName)
-        ? "mangas"
-        : "videos";
+    let FilesType = /\.(rar|zip)/gi.test(firstFile.FileName) ? "mangas" : "videos";
 
     if (!fs.existsSync(FolderCover)) {
-        let img = files.find((a) =>
-            /\.(jpg|jpeg|png|gif|webp)/i.test(a.FileName)
-        );
+        let img = files.find((a) => /\.(jpg|jpeg|png|gif|webp)/i.test(a.FileName));
         if (img) {
             try {
                 await sharp(path.join(dir, img.FileName))
@@ -100,11 +96,7 @@ const PopulateDB = async (files, FolderId, folder) => {
             } else {
                 if (f.Files.length > 0) {
                     console.log("folder: ", f.FileName);
-                    let result = await createFolderAndCover(
-                        f.FileName,
-                        f.Files,
-                        f
-                    );
+                    let result = await createFolderAndCover(f.FileName, f.Files, f);
                     if (result.Id) {
                         await PopulateDB(f.Files, result.Id, result.folder);
                     }
@@ -159,6 +151,13 @@ const removeOrphanFiles = async (Id, isFolder) => {
             }
         }
     }
+
+    let folders = await db.folder.findAll();
+    for (let f of folders) {
+        if (!fs.existsSync(f.Path)) {
+            await f.destroy();
+        }
+    }
 };
 const scanDirectory = async ({ id, dir, isFolder }) => {
     await removeOrphanFiles(id, isFolder);
@@ -193,10 +192,7 @@ const processJobs = async () => {
         try {
             let data = pendingJobs.pop();
             await scanDirectory(data);
-            await db.directory.update(
-                { IsLoading: false },
-                { where: { Id: data.id } }
-            );
+            await db.directory.update({ IsLoading: false }, { where: { Id: data.id } });
             process.send(data);
         } catch (err) {
             console.log("folder-scan line:135", err);
