87ce0fda7f1ce4de50b1f4f18e4d91f3cf004647
royel <royelconsoro@hotmail.com>
2022-10-10 21:53:07 -0400

    little change

--- db.js
+++ db.js
@@ -4,18 +4,14 @@ import fs from "fs-extra";
 const updateDb = async () => {
   await db.init();
   let i = 0;
-  const folders = await db.folder.findAll({ order: ["Name"] });
   const update = async (items) => {
     for (let d of items) {
-      const found = folders.find((f) => f.Name === d.Name);
-      if (found) {
-        console.log(found.Name);
-        await found.update(d);
-        i++;
-      }
+      console.log(d.Name);
+      await db.folder.update(d, { where: { Name: d.Name, FilesType: "mangas" } });
+      i++;
     }
   };
-  await update(fs.readJsonSync("F:\\db1.json"));
+  // await update(fs.readJsonSync("F:\\db1.json"));
   await update(fs.readJsonSync("F:\\db2.json"));
   console.log("updates", i);
 };
