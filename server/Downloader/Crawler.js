69dfeedf7ba018b0860b1e00d0c5f66571e45f28
royel21 <royelconsoro@hotmail.com>
2024-06-20 08:43:06 -0400

    fix error

--- server/Downloader/Crawler.js
+++ server/Downloader/Crawler.js
@@ -1,5 +1,6 @@
 import puppeteer from "puppeteer-extra";
 import puppeteerCore from "puppeteer-core";
+import os from "node:os";
 
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 puppeteer.use(StealthPlugin());
