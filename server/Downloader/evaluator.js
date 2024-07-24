ceba1eb9690d9fe80dfb32f8a18f2f645cfd0f27
royel21 <royelconsoro@hotmail.com>
2024-07-24 16:06:26 -0400

    some fix

--- server/Downloader/evaluator.js
+++ server/Downloader/evaluator.js
@@ -424,7 +424,7 @@ export const evaleLinks = async (query) => {
     const { src, dataset } = img;
     let nSrc = (dataset.lazySrc || dataset.src || dataset.lzlSrc || src)?.trim();
 
-    if (/\.(ico|svg)/.test(nSrc) || /manhwa-freak.com(.*).gif|.svg$/gi.test(nSrc)) continue;
+    if (/\.(ico|svg)|banner/.test(nSrc) || /manhwa-freak.com(.*).gif|.svg$/gi.test(nSrc)) continue;
 
     if (location.href.includes("mangaclash")) {
       if (nSrc.includes("mangaclash")) {
