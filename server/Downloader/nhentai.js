d1e74ce15c6f98ca99c192713baf3e04a4da4983
royel21 <royelconsoro@hotmail.com>
2024-07-30 14:17:34 -0400

    more fix

--- server/Downloader/nhentai.js
+++ server/Downloader/nhentai.js
@@ -144,12 +144,16 @@ export const downloadNHentai = async (link, page, server, state) => {
         try {
           const img = await getImgNh(url, page);
           if (img) {
+            if (meta.width > 1024) {
+              await img.resize({ width: 1024 });
+            }
+            if (i === 0 && !fs.existsSync(cover)) {
+              await saveThumbnail(buff, cover);
+            }
+
             const buff = await img.toFormat("jpg").toBuffer();
             zip.addFile(newImg, buff);
             count++;
-            if (i === 0 && !fs.existsSync(cover)) {
-              await saveThumbnail(img, cover);
-            }
           }
           f = 0;
           i++;
