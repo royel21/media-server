4a36b44d9638591bdb3de662c3916094a138faa0
royel21 <royelconsoro@hotmail.com>
2025-04-03 08:32:26 -0400

    more changes

--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 import fs from "fs-extra";
 
 const list = async () => {
-  const folders = fs.readdirSync("/mnt/5TB/").filter((f) => !/Anime|R18/i.test(f));
+  const folders = fs.readdirSync("/mnt/5TBHDD/").filter((f) => !/Anime|R18/i.test(f));
 
   for (let folder of folders) {
     console.log("Moving", folder);
