b88f142b4199e129d51760c220276ba6b22f03f5
royel21 <royelconsoro@hotmail.com>
2025-04-28 19:30:47 -0400

    more fix

--- videoconvert.js
+++ videoconvert.js
@@ -4,6 +4,15 @@ import path from "path";
 import Ffmpeg from "fluent-ffmpeg";
 import { exec } from "child_process";
 
+export function formatTime(time) {
+  if (time === 0) return "00:00";
+
+  let h = Math.floor(time / 3600);
+  let min = Math.floor((time / 3600 - h) * 60);
+  let sec = Math.floor(time % 60);
+  return (h === 0 ? "" : h + ":") + String(min).padStart(2, "0") + ":" + String(sec).padStart(2, "0");
+}
+
 async function getMetadata(filePath) {
   return new Promise((resolve, reject) => {
     exec(`ffprobe -v error -show_format -show_streams -print_format json "${filePath}"`, (error, stdout, stderr) => {
@@ -29,20 +38,22 @@ const convertVideo = async (vPath, isAnime) => {
   const files = fs.readdirSync(vPath).filter((f) => /\.(mp4|mkv)/i.test(f));
   let i = 0;
 
-  if (!fs.existsSync(path.join(vPath, "videos"))) {
-    fs.mkdirsSync(path.join(vPath, "videos"));
+  if (!fs.existsSync(path.join("../", "Videos"))) {
+    fs.mkdirsSync(path.join("../", "Videos"));
   }
   const padding = files.length.toString().length;
 
   for (let file of files) {
     await new Promise(async (resolve) => {
-      const toFile = path.join(vPath, "videos", file.replace(/mkv|webm/i, "mp4"));
+      const toFile = path.resolve("../", "Videos", file.replace(/mkv|webm/i, "mp4"));
       const current = `${(i + 1).toString().padStart(padding, "0")}/${files.length}`;
 
       const filePath = path.join(vPath, file);
 
       const meta = await getMetadata(filePath);
       let resize = meta?.streams[0]?.width > 1280;
+
+      const start = new Date().getTime();
 
       Ffmpeg(filePath)
         .audioBitrate("128k")
@@ -55,11 +66,13 @@ const convertVideo = async (vPath, isAnime) => {
           console.log(`${getime()} ~ ${data.duration} ~ ${str ? `${str.width}x${str.height} ~   ` : ""}${file}`);
         })
         .on("progress", (p) => {
-          process.stdout.write(`\r${current} ~ ${p.timemark} ~ ${p.percent.toFixed(2)}%  `);
+          const elapse = (new Date().getTime() - start) / 1000;
+          const percent = p.percent.toFixed(2);
+          process.stdout.write(`\r${current} ~ ${p.timemark} ~ ${percent}% ~ Elapse: ${formatTime(elapse)}`);
         })
         .saveToFile(toFile)
         .on("end", () => {
-          console.log(`${getime()} Save to:  ${toFile}`);
+          console.log(`${getime()} Save to:  ${toFile}\n`);
           resolve(true);
           // fs.removeSync(toFile);
         })
