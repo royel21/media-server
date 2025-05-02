295c7ef686f5bccc615028359610dc26426c8bf8
royel21 <royelconsoro@hotmail.com>
2025-05-02 14:45:33 -0400

    more fix

--- backup.sh
+++ backup.sh
@@ -4,11 +4,11 @@ BACKUP_DIR=$HOME"/backups"
 mkdir -p $BACKUP_DIR
 #save date in YYYY-MM-DD_HH-MM-SS format
 # This is the format used by mysqldump for the backup file name
-DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+DATE=$(date +"%Y-%m-%d")
 # Database Name
 DB_NAME="mediaserverdb"
 #dump the database to a file
 # The -u option specifies the MySQL user, -p specifies the password, and the database name is provided at the end
-mysqldump --defaults-extra-file=$HOME"/.secret.cnf" $DB_NAME > "$BACKUP_DIR/$DB_NAME_$DATE.sql"
+mysqldump --defaults-extra-file="$HOME/.secret.cnf" $DB_NAME > "${BACKUP_DIR}/${DB_NAME}_${DATE}.sql"
 # Clean up backups older than 30 days (optional)
 find $BACKUP_DIR -type f -name "*.sql" -mtime +7 -exec rm -f {} \;
