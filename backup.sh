317d0fc005cbac5bea4668c9c6b325c430a43e25
royel21 <royelconsoro@hotmail.com>
2025-05-02 13:51:12 -0400

    add script for db backup

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BACKUP_DIR=$HOME"/backups"
+# Create backup directory if it doesn't exist
+mkdir -p $BACKUP_DIR
+#save date in YYYY-MM-DD_HH-MM-SS format
+# This is the format used by mysqldump for the backup file name
+DATE=$(date +"%Y-%m-%d_%H-%M-%S")
+# Database Name
+DB_NAME="mediaserverdb"
+#dump the database to a file
+# The -u option specifies the MySQL user, -p specifies the password, and the database name is provided at the end
+mysqldump --defaults-extra-file=$HOME"/.secret.cnf" $DB_NAME > "$DB_NAME_$DATE.sql"
+# Clean up backups older than 30 days (optional)
+find $BACKUP_DIR -type f -name "*.sql" -mtime +7 -exec rm -f {} \;
