2dcf58e05938deda46fc5f5ddf9f5c9f54ab99d2
royel21 <royelconsoro@hotmail.com>
2025-07-28 06:23:12 -0400

    some fix

--- backupdata.sh
+++ backupdata.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -avu --progress --delete /mnt/2TBSSD /mnt/18TBHDD/2TBSSD
-rsync -avu --progress --delete /mnt/1TBSSD /mnt/18TBHDD/1TBSSD
-rsync -avu --progress --delete /mnt/5TBHDD /mnt/18TBHDD/5TBHDD
+rsync -avu --delete /mnt/1TBSSD /mnt/18TBHDD/
+rsync -avu --delete /mnt/2TBSSD /mnt/18TBHDD/
+rsync -avu --delete /mnt/5TBHDD /mnt/18TBHDD/
