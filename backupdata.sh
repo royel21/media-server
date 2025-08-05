96ea1d35a5cf165ac56fa1a52b9af37a92448e1f
royel21 <royelconsoro@hotmail.com>
2025-08-05 19:27:07 -0400

    more fix

--- backupdata.sh
+++ backupdata.sh
@@ -2,4 +2,7 @@
 
 rsync -avu --delete /mnt/1TBSSD /mnt/18TBHDD/
 rsync -avu --delete /mnt/2TBSSD /mnt/18TBHDD/
-rsync -avu --delete /mnt/5TBHDD /mnt/18TBHDD/
+rsync -avu --delete /mnt/5TBHDD/R18 /mnt/18TBHDD/5TBHDD
+#rsync -avu --delete /mnt/5TBHDD/R18-2 /mnt/18TBHDD/5TBHDD
+rsync -avu --delete /mnt/5TBHDD/Completed /mnt/18TBHDD/5TBHDD
+rsync -avu --delete /mnt/5TBHDD/Movies\ And\ Ovas /mnt/18TBHDD/5TBHDD
