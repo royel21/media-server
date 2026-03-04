df1fa8361c3e636445c75ab2e4f1e1dcae0b7d0e
royel21 <royelconsoro@hotmail.com>
2026-03-04 11:10:01 -0400

    somefix

--- #registry_game.bat
+++ #registry_game.bat
@@ -13,12 +13,12 @@ rem Add the "SaveDir" string value
 reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "SaveDir" /t REG_SZ /d "%folder%\savedata" /f
 
 rem Add the "InstallKey" string value
-reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "SaveDir" /t REG_SZ /d "CF73QX7IIMW" /f
+reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "InstallKey" /t REG_SZ /d "CF73QX7IIMW" /f
 
 rem Add the "ActMail" string value
 reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "ActMail" /t REG_SZ /d "always@smi.le" /f
 
 rem Add the "ActKey" string value
-reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "ActKey" /t REG_SZ /d "CF73QX7IIMW" /f
+reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "ActKey" /t REG_SZ /d "pastekey" /f
 
 endlocal
