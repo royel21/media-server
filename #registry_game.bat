42d783fb6fca9efe2bbef1b3405d42a541ce2d4c
royel21 <royelconsoro@hotmail.com>
2026-03-04 10:38:47 -0400

    temp batfile

--- #registry_game.bat
+++ #registry_game.bat
@@ -6,11 +6,19 @@ rem Get the current folder path
 set "folder=%~dp0"
 if "%folder:~-1%"=="\" set "folder=%folder:~0,-1%"
 
-rem Add the "InstMode" DWORD value (0)
-reg add "HKCU\SOFTWARE\ボンボンカンパニー\貧は僕らの福の神" /v "InstMode" /t REG_DWORD /d 0 /f
+rem Add the "DataDir" string value
+reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "DataDir" /t REG_SZ /d "%folder%" /f
 
-rem Add the "InstPath" string value
-reg add "HKCU\SOFTWARE\ボンボンカンパニー\貧は僕らの福の神" /v "InstPath" /t REG_SZ /d "%folder%" /f
+rem Add the "SaveDir" string value
+reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "SaveDir" /t REG_SZ /d "%folder%\savedata" /f
 
+rem Add the "InstallKey" string value
+reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "SaveDir" /t REG_SZ /d "CF73QX7IIMW" /f
+
+rem Add the "ActMail" string value
+reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "ActMail" /t REG_SZ /d "always@smi.le" /f
+
+rem Add the "ActKey" string value
+reg add "HKCU\SOFTWARE\CIRCUS\CDCD2" /v "ActKey" /t REG_SZ /d "CF73QX7IIMW" /f
 
 endlocal
