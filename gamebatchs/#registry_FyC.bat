42937fd1c4b5a6f2889755ab6d01fe9e369515a3
royel21 <royelconsoro@hotmail.com>
2026-06-12 13:09:34 -0400

    more changes

--- gamebatchs/#registry_FyC.bat
+++ gamebatchs/#registry_FyC.bat
@@ -6,19 +6,21 @@ rem Get the current folder path
 set "folder=%~dp0"
 if "%folder:~-1%"=="\" set "folder=%folder:~0,-1%"
 
-rem Add the "SetupFlag" DWORD value (1)
-reg add "HKCU\SOFTWARE\F&C\ADWin32\RAINBOW" /v "SetupFlag" /t REG_DWORD /d 1 /f
+set "CompanyKey=HKCU\SOFTWARE\F&C\ADWin32\RAINBOW"
 
-rem Add the "Group1" string value
-reg add "HKCU\SOFTWARE\F&C\ADWin32\RAINBOW" /v "Group1" /t REG_SZ /d "%folder%" /f
+rem Add the "Directory" string value (1)
+reg add %CompanyKey% /v "Directory" /t REG_SZ /d "%folder%" /f
 
-rem Add the "Group2" string value
-reg add "HKCU\SOFTWARE\F&C\ADWin32\RAINBOW" /v "Group2" /t REG_SZ /d "%folder%" /f
+rem Add the "Execute" string value
+reg add %CompanyKey% /v "Execute" /t REG_SZ /d "%folder%\ADVWIN32.EXE" /f
 
-rem Add the "Group3" string value
-reg add "HKCU\SOFTWARE\F&C\ADWin32\RAINBOW" /v "Group3" /t REG_SZ /d "%folder%" /f
+rem Add the "Install" DWORD value (1)
+reg add %CompanyKey% /v "Install" /t REG_DWORD /d /d 1 /f
 
-rem Add the "GroupPath" string value
-reg add "HKCU\SOFTWARE\F&C\ADWin32\RAINBOW" /v "GroupPath" /t REG_SZ /d "%folder%" /f
+rem Add the "Uninstall" string value
+reg add %CompanyKey% /v "Uninstall" /t REG_SZ /d "%folder%\UNINST.EXE" /f
+
+rem Add the "UserCount" DWORD value (1)
+reg add %CompanyKey% /v "UserCount" /t REG_DWORD /d 1 /f
 
 endlocal
