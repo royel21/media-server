01a497dbb97af858d91796ca21c65fac70c47a4d
royel21 <royelconsoro@hotmail.com>
2026-06-16 18:52:21 -0400

    more fix

--- src/admin/Games/GameTabs.svelte
+++ src/admin/Games/GameTabs.svelte
@@ -16,7 +16,10 @@
   let filter = localStorage.getItem("gamelist-filter") || "";
 
   const loadGames = async () => {
-    const data = await apiUtils.admin(["games", pageData.page, calRows(), filter], "g-list");
+    const data = await apiUtils.admin(
+      ["games", pageData.page, calRows(), filter],
+      "g-list",
+    );
 
     Games = data.items || [];
     pageData.totalItems = data.totalItems || 0;
@@ -60,7 +63,11 @@
       filter,
       rows: calRows(),
     };
-    const result = await apiUtils.post("admin/games/remove-game", data, "up-data");
+    const result = await apiUtils.post(
+      "admin/games/remove-game",
+      data,
+      "up-data",
+    );
 
     Games = result.items || [];
     pageData.totalItems = result.totalItems || 0;
@@ -113,6 +120,10 @@
                       .join(", ")
                       .trim();
                 }
+
+                if (g.Info.AltName === "-") {
+                  g.Info.AltName = "N/A";
+                }
               }
               let devRegex = /^Developer( :|) /;
               if (devRegex.test(p)) {
@@ -152,7 +163,16 @@
 
 <div class="admin-manager">
   <div class="rows">
-    <GameList {Games} {game} {setInfo} {pageData} {filter} {gotopage} {filterChange} {addGame} />
+    <GameList
+      {Games}
+      {game}
+      {setInfo}
+      {pageData}
+      {filter}
+      {gotopage}
+      {filterChange}
+      {addGame}
+    />
     <GameInfo bind:game {updateGame} {removeGame} />
   </div>
 </div>
