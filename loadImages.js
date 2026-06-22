c57a06487ab4eea6a80f399424f9af42ebc1cc44
royel21 <royelconsoro@hotmail.com>
2026-06-22 14:24:28 -0400

    more fix

--- loadImages.js
+++ loadImages.js
@@ -32,7 +32,7 @@ const worker = async () => {
     game.Codes = game.Codes.trim();
 
     console.log(
-      `${i + 1}/${games.length}`.padStart(10) + ": " + "Codes: " + game.Codes + " - " + game.Game?.Name || "",
+      `${i++ + 1}/${games.length}`.padStart(10) + ": " + "Codes: " + game.Codes + " - " + game.Game?.Name || "",
     );
     if (/^v\d+$/.test(game.Codes || "")) {
       if (containAssianChar.test(game.AltName || "")) {
