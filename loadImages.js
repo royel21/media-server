b42f3f8335729421a7d491226cc4b69d3a89dc85
royel21 <royelconsoro@hotmail.com>
2026-07-08 09:08:47 -0400

    more fix

--- loadImages.js
+++ loadImages.js
@@ -96,6 +96,7 @@ export function sortAsianFirst(arr) {
 
 const worker = async () => {
   const games = await db.Info.findAll({
+    attributes: ["Name", "AltName", "Codes"],
     where: { Codes: { [db.Op.like]: "v%" } },
     include: [{ model: db.Game, required: true, attributes: ["Name"] }],
   });
